1575520894464b6ff611064f5bfd8f1cfaba3837
arsiiee <azsalcedo64143@liceo.edu.ph>
2026-08-02 01:37:51 +0800

    Lab Activity 5: State and Recomposition

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme
+val Maroon40 = Color(0xFF7B1113)
+val MaroonGrey40 = Color(0xFF9A4A4A)
+val Rose40 = Color(0xFFB86C6C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme
+val Maroon80 = Color(0xFFE9C9C9)
+val MaroonGrey80 = Color(0xFFDDB8B8)
+val Rose80 = Color(0xFFF4DEDE)
